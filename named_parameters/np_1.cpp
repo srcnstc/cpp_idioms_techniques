9221defa9dd9972889bfe1ad401128ccbb24ae3e
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-02-01 13:11:17 +0300

    Create np_1.cpp

---
+++ named_parameters/np_1.cpp
@@ -0,0 +1,13 @@
+#include <string>
+
+class File {
+public:
+    File(std::string file_name, bool m_readonly, bool create_if_not_exists, unsigned block_size);
+//...
+};
+
+
+int main()
+{
+    File myfile("deneme.txt", true, false, 1024);
+}
