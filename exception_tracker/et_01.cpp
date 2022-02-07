e4d0b5f3e0e46131e5fb9b314f2ef766bb538aa2
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-02-07 10:59:10 +0300

    Create et_01.cpp

---
+++ exception_tracker/et_01.cpp
@@ -0,0 +1,53 @@
+#include <stdexcept>
+#include <iostream>
+#include <cassert>
+
+struct X {
+    X(int) 
+    {
+        throw std::runtime_error("X error"); 
+    }
+};
+
+struct Y {
+    Y(int) 
+    {
+        throw std::runtime_error("Y error"); 
+    }
+};
+
+class Nec {
+private:
+    X mx;
+    Y my;
+    enum Tracker { NONE, FIRST, SECOND };
+public:
+    Nec(Tracker tracker = NONE)
+        try    
+        : mx((tracker = FIRST, 1)) 
+        , my((tracker = SECOND, 2)) 
+    {
+        assert(tracker == SECOND);
+        // ... constructor body ...
+    }
+    catch (const std::exception& e)
+    {
+        if (tracker == FIRST) {
+            std::cout << "X threw: " << e.what() << '\n';
+        }
+        else if (tracker == SECOND) {
+            std::cout << "Y threw: " << e.what() << '\n';
+        }
+        throw;
+    }
+};
+
+int main()
+{
+    try {
+        Nec nec;
+    }
+    catch (const std::exception &e) {
+        std::cout << "exceptşon caught: " << e.what() << std::endl;
+    }
+}
