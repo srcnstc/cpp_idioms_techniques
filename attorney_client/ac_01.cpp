0802d1163a27fb6f9ac9db0d07235e01d90725a2
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-02-17 16:00:51 +0300

    Create ac_01.cpp

---
+++ attorney_client/ac_01.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+
+class Nec {
+public:
+    void print()const
+    {
+        std::cout << "mx = " << mx << " " << " my = " << my << "\n";
+    }
+private:
+    int mx{}, my{};
+
+    void pfunc1()
+    {
+        std::cout << "pfunc1 cagrildi\n";
+    }
+
+    void pfunc2()
+    {
+        std::cout << "pfunc2 cagrildi\n";
+    }
+
+    void pfunc3()
+    {
+        std::cout << "pfunc3 cagrildi\n";
+    }
+
+    friend class Helper;
+};
+
+class Helper {
+    static void pfunc1(Nec& rnec)
+    {
+        rnec.pfunc1();
+    }
+
+    static int get_mx(const Nec& rnec)
+    {
+        return rnec.mx;
+    }
+    friend class Erg;
+};
+
+class Erg {
+public:
+    void foo(Nec& r)
+    {
+        Helper::pfunc1(r);
+        auto a{ Helper::get_mx(r) };
+        std::cout << "a = " << a << "\n";
+
+    }
+};
+
+int main()
+{
+    Erg erg;
+    Nec nec;
+
+    erg.foo(nec);
+}
